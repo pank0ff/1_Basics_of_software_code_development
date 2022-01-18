25f6b6144cda3dc0fc84358ac4f19631dae91cd0
Sergey Rundygin <90576203+pank0ff@users.noreply.github.com>
2022-01-18 23:36:50 +0300

    Add files via upload

---
+++ linear/src/main/java/com/rundygin/linear/SixthTask.java
@@ -0,0 +1,8 @@
+package com.rundygin.linear;
+
+public class SixthTask {
+     static public void calc(double x,double y){
+         System.out.println((Math.abs(x)<=2 && Math.abs(y-2)<=2) || (Math.abs(x)<=4 && Math.abs(y+1.5f)<=1.5f));
+    }
+    }
+
