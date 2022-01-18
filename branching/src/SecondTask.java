4f0aef3682f02a1da574075cc6dfeccab1c85bb5
Sergey Rundygin <90576203+pank0ff@users.noreply.github.com>
2022-01-19 00:13:19 +0300

    Delete SecondTask.java

--- branching/src/SecondTask.java
+++
@@ -1,9 +0,0 @@
-public class SecondTask {
-    public static void calc(double a, double b, double c, double d) {
-        double max;
-        if (a > b){}else{b=a;}
-        if (c > d){}else{d=c;}
-        if (d > b){max = d;}else{max = b;}
-        System.out.println(max);
-    }
-}
